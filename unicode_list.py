53640c7916cc9e752eca67de0c538bbfbab3bbc4
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-16 17:16:32 +0530

    Update unicode_list.py

--- unicode_list.py
+++ unicode_list.py
@@ -6,12 +6,12 @@ S = 'mumbai'
 unicode_S = []
 for i in S:
     unicode_S.append(ord(i))
-print(unicode_S)
+print("Using list methods: ", unicode_S)
 
 # Using List comprehension
 unicode_s = [ord(x) for x in S]
-print(unicode_s)
+print("Using list comprehension: ", unicode_s)
 
 # Using map() function
 unicode = map(lambda x: ord(x), S)
-print(list(unicode))
+print("Using map function: ", list(unicode))
